2c4a4c7d028525dd7ea18f979ecf09108f6d1735
Mateo Valenzuela <valenzuelamateo98@gmail.com>
2025-10-26 22:49:43 -0400

    alfin

--- gateway/src/main/java/com/enseniamelo/gateway/config/SecurityConfig.java
+++ gateway/src/main/java/com/enseniamelo/gateway/config/SecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.security.web.server.authentication.RedirectServerAuthenticationSuccessHandler;
 
 @Configuration
 @EnableWebFluxSecurity
@@ -19,11 +20,16 @@ public class SecurityConfig {
                 .pathMatchers("/api/v1/auth/**").permitAll()
                 .pathMatchers("/actuator/**").permitAll()
                 .pathMatchers("/eureka/**").permitAll()
-                .pathMatchers("/openapi/**", "/swagger-ui/**", "/v3/api-docs/**", "/webjars/**").permitAll()
-                
+                .pathMatchers("/openapi/**", "/swagger-ui/**", "/swagger-ui.html", "/v3/api-docs/**", "/webjars/**").permitAll()
+                .pathMatchers("/login/**", "/oauth2/**").permitAll()
                 .anyExchange().authenticated()
             )
-            .oauth2ResourceServer(oauth2 -> oauth2.jwt(jwt -> {}));
+            .oauth2Login(oauth2 -> oauth2
+                .authenticationSuccessHandler(new RedirectServerAuthenticationSuccessHandler("http://localhost:5173"))
+            )
+            .oauth2ResourceServer(oauth2 -> oauth2
+                .jwt(jwt -> {})
+            );
         
         return http.build();
     }
